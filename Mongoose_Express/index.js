3d4ae3cce27de782784f8ab8573bd9e0650b5346
notPesol <pesol2012@hotmail.com>
2021-06-23 11:43:24 +0700

    add delete & filter by category on MongooseExpress

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -24,15 +24,28 @@ app.use(express.urlencoded({ extended: true }));
 // use method override
 app.use(methodOverride('_method'))
 
+
+const categories = ['fruit', 'vegetable', 'daily', 'mushroom', 'watch', 'computer'];
+
+
 app.get('/products', async (req, res) => {
-    const products = await Product.find({});
-    res.render('products/index', { products });
+    const { category } = req.query
+
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category, categories });
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: 'All', categories });
+    }
+
     console.log(`You are in ${req.url}`);
 });
 
+
 // route for form
 app.get('/products/new', (req, res) => {
-    res.render('products/new');
+    res.render('products/new', { categories });
     console.log(`You are in ${req.url}`);
 });
 
@@ -54,7 +67,7 @@ app.get('/products/:id', async (req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const foundProduct = await Product.findById(id);
-    res.render('products/edit', { product: foundProduct });
+    res.render('products/edit', { product: foundProduct, categories });
     console.log(`You are in ${req.url}`);
 })
 // route for use update
@@ -63,6 +76,15 @@ app.put('/products/:id', async (req, res) => {
     const product = await Product.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
     res.redirect('/products/' + product._id)
     console.log(product);
+
+})
+
+// route for delete product
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deleteProduct = await Product.findByIdAndDelete(id);
+    console.log('Deleted', deleteProduct, 'success!');
+    res.redirect('/products');
 
 })
 
